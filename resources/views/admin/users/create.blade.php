91d85ff80196fc8f354dda90999703d037c3d5af
KunYou Wang <stam987654@gmail.com>
2017-08-23 18:35:20 +0800

    users with uploading feature working

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -25,8 +25,8 @@
     </div>
 
     <div class="form-group">
-        {!! Form::label('file', 'Title:') !!}
-        {!! Form::file('file') !!}
+        {!! Form::label('photo_id', 'Photo:') !!}
+        {!! Form::file('photo_id') !!}
     </div>
 
     <div class="form-group">
